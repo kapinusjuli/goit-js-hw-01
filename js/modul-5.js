b266414564a71a464d816231b5b47fd687301eec
Капинус Юлия <kapinusjuli55@gmail.com>
2020-09-07 19:14:00 +0300

    дз6

--- js/modul-5.js
+++ js/modul-5.js
@@ -255,7 +255,7 @@ class Car {
    * Метод, который заводит автомобиль
    * Записывает в свойство `isOn` значение `true`
    */
-  turnOn() {}
+  //turnOn() {}
 
   /*
    * Метод, чтобы заглушить автомобиль
@@ -316,20 +316,46 @@ class Car {
 // }
 // */
 
-
 class Car {
   // Write code under this line
-   getSpecs(){}
-   constructor(){}
-   get price() {}
-   set price(value) {}
-   turnOn() {}
-   turnOff() {}
-   accelerate(value) {}
-   decelerate(value) {}
-   drive(hours) {}
- }
   
+   static getSpecs(car){
+     // car = {maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000}; 
+     return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`;
+      }
+    constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0} = {}){
+      this.speed = speed;
+      this._price = price;
+      this.maxSpeed = maxSpeed;
+      this.isOn = isOn;
+      this.distance = distance;
+         }
+    get price() {
+      return this._price;
+    }
+    set price(value) {
+     this._price = value;
+    }
+    turnOn() {
+      this.isOn = true;
+    }
+    turnOff() {
+     this.isOn = false;
+     this.speed = 0;
+    }
+    accelerate(value) {
+      if ((this.speed + value) < this.maxSpeed) {this.speed += value} else {this.speed = this.maxSpeed};
+ 
+    }
+     decelerate(value) {
+     let tempSpeed = this.speed - value;
+     this.speed - value >= 0 ? this.speed -= value : this.speed = 0; 
+    }
+    drive(hours) {
+      return this.distance += hours * this.speed;
+    }
+  
+   } 
  const mustang = new Car({ maxSpeed: 200, price: 2000 });
  mustang.turnOn();
  mustang.accelerate(50);
@@ -345,7 +371,7 @@ class Car {
   console.log(Car.getSpecs(mustang));
  // 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
  
- // console.log(mustang.price); // 2000
+  console.log(mustang.price); // 2000
  mustang.price = 4000;
   console.log(mustang.price); // 4000
  
